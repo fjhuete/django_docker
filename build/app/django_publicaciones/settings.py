354ab83162263c2c6f56224f119b6e6d975cde34
Francisco Javier Huete <fjhuete.m@gmail.com>
2025-02-11 13:17:48 +0000

    Añadido el parámetro trusted origins y la variable URL_BASE

--- build/app/django_publicaciones/settings.py
+++ build/app/django_publicaciones/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [os.environ.get("URL_BASE"),]
